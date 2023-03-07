91a244d08386caae992244574bcc073a5ba1e089
Gareth <g3vans16@gmail.com>
2023-03-07 11:39:21 +0000

    fixed stack overflow

--- employeeservice/src/main/java/com/codeclan/example/employeeservice/controllers/AssignmentController.java
+++ employeeservice/src/main/java/com/codeclan/example/employeeservice/controllers/AssignmentController.java
@@ -7,10 +7,12 @@ import com.codeclan.example.employeeservice.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
 public class AssignmentController {
 
     @Autowired
--- employeeservice/src/main/java/com/codeclan/example/employeeservice/models/Assignment.java
+++ employeeservice/src/main/java/com/codeclan/example/employeeservice/models/Assignment.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.employeeservice.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 
 @Entity
@@ -15,10 +17,12 @@ public class Assignment {
     @ManyToOne
     @JoinColumn(name = "employee_id")
     private Employee employee;
+    @JsonIgnoreProperties({"assignments"})
 
     @ManyToOne
     @JoinColumn(name = "project_id")
     private Project project;
+    @JsonIgnoreProperties({"assignments"})
 
     public Assignment() {}
 
